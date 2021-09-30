c64410dc8efbda163fdfc66e198ce29ea9b19ce0
alessandrochillotti <alessandro.chillotti@outlook.it>
2021-09-30 22:53:04 +0200

    Consentiti file sh e modificati

--- app/clean_all.sh
+++ app/clean_all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-docker network rm app_1_my_net
+docker network rm app_${1}_my_net
 
-docker rm app_1_node_1
-docker rm app_1_node_2
-docker rm app_1_node_3
-docker rm app_1_sequencer_node_1
-docker rm app_1_register_node_1
+docker rm app_${1}_node_1
+docker rm app_${1}_node_2
+docker rm app_${1}_node_3
+docker rm app_${1}_sequencer_node_1
+docker rm app_${1}_register_node_1
 
-docker rmi app_1_node
-docker rmi app_1_sequencer_node
-docker rmi app_1_register_node
+docker rmi app_${1}_node
+docker rmi app_${1}_sequencer_node
+docker rmi app_${1}_register_node
--- app/stop.sh
+++ app/stop.sh
@@ -8,7 +8,7 @@ fi
 
 if [ $1 -eq 1 ] || [ $1 -eq 2 ] || [ $1 -eq 3 ]
 then
-    cd app_${1} && docker-compose -f algorithm-${1}.yml down && ./../clean_all.sh
+    cd app_${1} && docker-compose -f algorithm-${1}.yml down && ./../clean_all.sh ${1}
 else
     echo -e "The algorithm must be 1, 2 or 3."
 fi
