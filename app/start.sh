f339937189ed7b537c454b44276740902c9d07bc
alessandrochillotti <alessandro.chillotti@outlook.it>
2021-10-11 23:26:02 +0200

    L'instanziazione dell'applicazione avviene in background

--- app/start.sh
+++ app/start.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # if less than two arguments supplied, display usage 
-if [ $# -ne 1 ] 
+if [ $# -ne 2 ] 
 then 
-    echo "Usage: ${0} ALGO"
+    echo "Usage: ${0} ALGO NUMBER_NODE"
     exit 1
 fi
 
 if [ $1 -eq 1 ]
 then
-    ALGO=1 docker-compose --profile sequencer up --scale node=3
+    ALGO=1 NUMBER_NODE=${2} docker-compose --profile sequencer up --scale node=3 # -d
 elif [ $1 -eq 2 ]
 then
-	ALGO=2 docker-compose --profile no_sequencer up --scale node=3
+	ALGO=2 NUMBER_NODE=${2} docker-compose --profile no_sequencer up --scale node=3 # -d
 elif [ $1 -eq 3 ]
 then
-	ALGO=3 docker-compose --profile no_sequencer up --scale node=3
+	ALGO=3 NUMBER_NODE=${2} docker-compose --profile no_sequencer up --scale node=3 # -d
 else
     echo -e "The algorithm must be 1, 2 or 3."
 fi
